400a90c14e017771718270e348450720eb8ceb0b
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-08-16 13:41:19 -0300

    aprendeno

--- Ycolecoes/Test/ListArrayConversaoTest01.java
+++ Ycolecoes/Test/ListArrayConversaoTest01.java
@@ -27,6 +27,6 @@ public class ListArrayConversaoTest01 {
         System.out.println(Arrays.toString(numbersArray));
         System.out.println(integerList);
 
-        System.out.println();
+        System.out.println("ola");
     }
 }
