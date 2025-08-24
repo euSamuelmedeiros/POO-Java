9f618f2ea0d408267baf061a5d2f7ee11a46e2a7
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-08-24 16:49:13 -0300

    Estudando hashMap

--- Ycolecoes/Test/MapTest01.java
+++ Ycolecoes/Test/MapTest01.java
@@ -1,8 +1,16 @@
 package Ycolecoes.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapTest01 {
     public static void main(String[] args) {
-        System.out.println("Hello word");
+        Map<String, String> map = new HashMap<>();
+
+        map.put("teklado", "teclado");
+        map.put("olla", "ola");
+        map.put("vc", "você");
+        System.out.println(map);
 
     }
 }
