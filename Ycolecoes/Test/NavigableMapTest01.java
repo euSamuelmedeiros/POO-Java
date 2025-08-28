5ba1d8517102518b0e01dda8bb08148db5cb6966
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-08-27 21:26:35 -0300

    Estudando e praticando NavigableMap

---
+++ Ycolecoes/Test/NavigableMapTest01.java
@@ -0,0 +1,21 @@
+package Ycolecoes.Test;
+
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class NavigableMapTest01 {
+    public static void main(String[] args) {
+        NavigableMap<String, String> map =  new TreeMap();
+
+        map.put("A", "Letra A");
+        map.put("D", "Letra D");
+        map.put("C", "Letra C");
+        map.put("B", "Letra B");
+
+        for (Map.Entry<String,String> entry : map.entrySet()){
+            System.out.println(entry.getKey() +"-"+ entry.getValue());
+        }
+            }
+}
