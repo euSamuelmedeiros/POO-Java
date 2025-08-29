08f1c1d7ae21ad596e9116c6897c9b88c50b14c8
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-08-28 21:53:09 -0300

    Estudando

--- Ycolecoes/Domain/SmartPhone.java
+++ Ycolecoes/Domain/SmartPhone.java
@@ -1,55 +1,6 @@
 package Ycolecoes.Domain;
 
 public class SmartPhone {
-    private String SerialNumber;
-    private String marca;
-
-    public SmartPhone(String serialNumber, String marca) {
-        SerialNumber = serialNumber;
-        this.marca = marca;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null)
-            return false;
-        if (this == obj) {
-            return true;
-        }
-        if (this.getClass() != obj.getClass())
-            return false;
-        SmartPhone smartPhone = (SmartPhone) obj;
-        return SerialNumber != null && SerialNumber.equals(smartPhone.SerialNumber);
-    }
-
-    @Override
-    public int hashCode(){
-        return this.SerialNumber    .hashCode();
-    }
-
-
-
-    public String getSerialNumber() {
-        return SerialNumber;
-    }
-
-    public void setSerialNumber(String serialNumber) {
-        SerialNumber = serialNumber;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    @Override
-    public String toString() {
-        return "SmartPhone{" +
-                "SerialNumber='" + SerialNumber + '\'' +
-                ", marca='" + marca + '\'' +
-                '}';
-    }
+    publ
+     }
 }
