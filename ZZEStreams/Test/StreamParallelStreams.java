41b9c844e392f8360a31139e4c247fa0c31b96af
euSamuelmedeiros <Samuelmedeiros1617@gmail.com>
2025-12-06 00:41:30 -0300

    .

--- ZZEStreams/Test/StreamParallelStreams.java
+++ ZZEStreams/Test/StreamParallelStreams.java
@@ -9,18 +9,10 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class StreamParallelStreams {
-    public static List<TV> tvList = new ArrayList<>(List.of(
-            new TV("Velozes e furiosos", CategoriaTV.ACAO, 2010),
-            new TV("Hora de aventura", CategoriaTV.DESENHO, 2016),
-            new TV("Clarencio ", CategoriaTV.DESENHO, 2019),
-            new TV("Gente Grande", CategoriaTV.DRAMA, 2012),
-            new TV("Terremoto em sanDiego", CategoriaTV.DRAMA, 2020),
-            new TV("Todo mundo em panico", CategoriaTV.ACAO, 2010)
-
-
-    ));
 
     public static void main(String[] args) {
+        System.out.println(Runtime
+                .getRuntime().availableProcessors());
 
     }
 }
